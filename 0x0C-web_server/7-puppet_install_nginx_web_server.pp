5e90413c2887535b4b5abc6aa6d9e7d269d6adc2
edwin karanja mburu <edwin@eddy-kali.browns.com>
2023-08-29 09:04:38 +0300

    fixing to pass checker requirements

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -14,28 +14,15 @@ service { 'nginx':
   require => package['nginx'],
 }
 
-# Configuring the redirection nginx server
-exec { 'configure-nginx-redirect':
-  command  => 'sed -i \'s/server_name _;\n\trewrite ^\/redirect_me https:\\/\\/www.youtube.com\\/watch?v=QH2-TGUlwu4 permanent \
-  /etc/nginx/sites-enabled/default,'
-  provider => shell,
+file_line { 'config':
+  ensure  => 'present',
+  path    => '/etc/nginx/sites-enabled/default',
+  after   => 'listen 80 default_server;',
+  line    => 'rewrite ^redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent',
 }
 
-# configuring nginx to listen on port 80
-exec { 'configure-nginx-listen':
-  command  => 'sed -i \'s/listen 80;/listen 80 default_server;/\' /etc/nginx/sites-available/default',
-  provider => 'shell',
-  require  => Package['nginx'],
-}
-
-# creatinag a page that returns Hello World!
+# creating a page that returns Hello World!
 exec {'Helloworld-file':
   command  => 'echo "Hello World!" > /var/www/html/index.html',
-  provider => shell,
-}
-
-# Restarting nginx
-exec {'restart':
-  command  => 'service nginx restart',
   provider => shell,
 }
