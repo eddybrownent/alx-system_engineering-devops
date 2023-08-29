516d1a0d7e6c2c9132a18832f32edcf8539bd53b
edwin karanja mburu <edwin@eddy-kali.browns.com>
2023-08-29 14:42:20 +0300

    configure an Nginx server using Puppet instead of Bash | changed the typo errors | added a new line to the .html output | changing a directory

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -28,7 +28,7 @@ exec { 'configure-nginx-listen':
 # creatinag a page that returns Hello World!
 file { '/var/www/html/index.nginx-debian.html':
   ensure  => present,
-  content => "Hello World!\n",
+  content => "Hello World!",
 }
 
 # Restarting nginx
