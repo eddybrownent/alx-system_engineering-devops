07cb05fa6f6c044a2ee60d577935a108054a4405
edwin karanja mburu <edwin@eddy-kali.browns.com>
2023-09-26 00:40:42 +0300

    Python script that, using a REST API, for a given employee ID, returns information about his/her TODO list progress.

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -4,37 +4,37 @@
 import sys
 import requests
 
-# Checking if script is being executed as main
+""" Checking if script is being executed as main """
 if __name__ == '__main__':
-    # extract employee ID from the command-line argument
+    """ extract employee ID from the command-line argument """
     em_id = sys.argv[1]
 
-    # Defining the base URL for the API
+    """ Defining the base URL for the API """
     baseUrl = "https://jsonplaceholder.typicode.com/users"
 
-    # Constructing the URL to fetch employee info
+    """ Constructing the URL to fetch employee info """
     url = baseUrl + "/" + em_id
 
-    # Send an HTTP GET request to get employee info
+    """Send an HTTP GET request to get employee info """
     response = requests.get(url)
 
-    # Extract the employee name from the JSON response
+    """ Extract the employee name from the JSON response """
     em_name = response.json().get('name')
 
-    # the URL for fetching the employee's todo list
+    """ the URL for fetching the employee's todo list """
     todoUrl = url + "/todos"
 
-    # Send an HTTP GET request to get the todo list
+    """ Send an HTTP GET request to get the todo list """
     response = requests.get(todoUrl)
 
-    # Extract the list of tasks from the JSON response
+    """ Extract the list of tasks from the JSON response """
     tasks = response.json()
 
-    # counters for completed tasks and a list to store them
+    """ counters for completed tasks and a list to store them """
     done = 0
     done_tasks = []
 
-    # Iterate through the tasks counting completed tasks and store
+    """ Iterate through the tasks counting completed tasks and store """
     for task in tasks:
         if task.get('completed'):
             done_tasks.append(task)
